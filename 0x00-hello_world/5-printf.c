bb87ce7b72d89d3f63b5a451a6ea4d2488b8e597
Imane Ouahmane <imane.ouahmane22@gmail.com>
2023-03-01 15:33:06 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h> 
+/*
+* main - A program that print in lin using the printf fonction
+* return: 0(success)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
