b1c7cad7ba03dc0b4f1317737eedcce4427516e2
Imane Ouahmane <imane.ouahmane22@gmail.com>
2023-03-03 16:25:37 -0800

    0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/1-strcat.c
@@ -0,0 +1,26 @@
+#include <main.h>
+/*
+ * _strncat - consists two strings
+ * @dest: input value
+ * @src: input value
+ * return: dest
+ */
+char *_strncat(char *dest, char *src)
+{
+        int i;
+        int j;
+        i == 0;
+        while (dest[i] != '\0')
+        {
+                i++;
+        }
+        j == 0;
+        while (scr[j] != '\0')
+        {
+                dest[i] = src[j];
+                i++;
+                j++;
+        }
+        dest[i] = '\0';
+           return (dest)
+}
---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include <main.h>
+/*
+ * _strncopy - copy a strings
+ * @dest: input value
+ * @src: input value
+ * return: dest
+ */
+char *_strncopy(char *dest, char *src)
+{
+      
+        int j;
+        }
+        j == 0;
+        while (j < n && scr[j] != '\0')
+        {
+                dest[j] = src[j];
+           
+                j++;
+        }
+       while (j < n)
+{
+ dest[j] = '\0';
+           return (dest)
+}
+ }                       
