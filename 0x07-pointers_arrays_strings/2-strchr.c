7e5295953579899bc613d2eb89a7b54a015a4b21
Imane Ouahmane <imane.ouahmane22@gmail.com>
2023-03-06 17:19:17 -0800

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/*
+ * _strvhr - Enter point 
+ * @s: input 
+ * @c: input
+ * return always 0 (success)
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0
+		for (;s[i] >= '\0'; i++)
+		{
+			if (s[i] = c)
+				return (&s[i])
+		}
+	return (0)
+}
